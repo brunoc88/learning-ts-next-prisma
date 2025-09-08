eecddc8e77b28181d74943a9fbf3ddf4a635f006
brunoc88 <brunocerutti88@gmail.com>
2025-09-08 14:11:27 -0300

    Update dia6.ts

--- src/dia6.ts
+++ src/dia6.ts
@@ -7,3 +7,61 @@ function identidad <T> (valor: T): T {
 
 console.log(identidad(12))
 console.log(identidad('hola')) 
+
+// 2. Par genérico
+// Crea una función `par` que acepte dos valores de tipos distintos 
+// y devuelva un objeto con ambas propiedades.
+
+function par <K, V> (clave:K, valor:V) {
+    return {clave, valor}
+}
+
+console.log(par('id',1))
+
+// 3. Caja genérica
+// Define una interfaz genérica `Caja<T>` que contenga un valor de tipo `T`.
+// Crea una caja de `number` y otra de `string`.
+
+interface Caja<T> {
+    contenido: T
+}
+
+let caja: Caja <number> = {
+    contenido: 10
+}
+let caja2: Caja <string> = {
+    contenido: 'RTX 5090'
+}
+console.log(caja)
+console.log(caja2)
+
+// 4. Array genérico
+// Crea una función `primero<T>` que devuelva el primer elemento de un array genérico.
+
+let primero = <T> (arr:T []) : T => arr[0]
+
+console.log(primero([14,22,10]))
+console.log(primero(['hola','dev','adios']))
+
+// 5. Constraining
+// Crea una función `obtenerLongitud` que solo acepte tipos que tengan la propiedad `length`.
+
+let obtenerLongitud = <T extends { length: number }>(value: T) : number =>  value.length
+console.log(obtenerLongitud('hola'))
+console.log(obtenerLongitud([1,2,3]))
+
+// 6. Combinar
+// Crea una función `combinar<T, U>` que acepte dos objetos y los combine en uno solo.
+
+let combinar = <T, U>(item: T, item2: U): T & U => {
+    return { ...item, ...item2 }
+}
+
+console.log(combinar({ id: 1 }, { nombre: "Bruno" }))
+
+/*Explicación rápida:
+
+T & U → usamos intersection type para decir que el resultado tendrá las propiedades de T y U.
+
+{ ...item, ...item2 } → hace un spread para unir ambos objetos.
+*/
